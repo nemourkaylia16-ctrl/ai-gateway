b3ed0b07f45add820833e07f511f76b4ca25becd
nemourkaylia16-ctrl <nemourkaylia16@gmail.com>
2026-02-02 11:49:52 +0100

    Create widget.js

---
+++ widget.js
@@ -0,0 +1,16 @@
+const socket = new WebSocket("https://ai-gateway-erqa.onrender.com");
+
+socket.onmessage = (e) => {
+  const data = JSON.parse(e.data);
+
+  if (data.type === "typing") showTyping();
+  if (data.type === "message") showBot(data.text);
+};
+
+function sendMessage(text) {
+  socket.send(JSON.stringify({
+    client_id: "ABC-123",
+    message: text,
+    language: "ar"
+  }));
+}
